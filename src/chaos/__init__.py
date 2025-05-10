08325482b6804bf4c17bb57409e2511d6ad2e0d6
Nathaniel Ledford <nate@nateledford.com>
2025-05-10 12:28:50 -0400

    Get basic api working

--- src/chaos/__init__.py
+++ src/chaos/__init__.py
@@ -1,2 +1,26 @@
-def main() -> None:
-    print("Hello from chaos!")
+import datetime
+import os
+import socket
+
+from robyn import Robyn
+
+app = Robyn(__file__)
+
+
+async def get_random_system_data() -> str:
+    """
+    Gets 32 bytes of random data from the host system and returns it as a hexadecimal string
+    """
+    return os.urandom(32).hex()
+
+
+@app.get("/")
+async def h(request):
+    return {
+        "host": socket.getfqdn(),
+        "timestamp": datetime.datetime.now(datetime.UTC),
+        "data": await get_random_system_data()
+    }
+
+
+app.start(port=8080)
